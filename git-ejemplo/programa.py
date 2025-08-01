575c24eb3e5076f6b564b0c5d035e45c59473e69
Diego Alexander Contreras Duarte <dcontrerasd>
2025-08-01 14:26:52 -0600

    Version 4: Agrega Validacion de edad

--- git-ejemplo/programa.py
+++ git-ejemplo/programa.py
@@ -1,4 +1,8 @@
 print("Bienvenido al programa de Diego")
 nombre = input("¿Cómo te llamas? ")
-edad = input("¿Cuántos años tienes? ")
-print("Hola", nombre, "veo que tienes", edad, "años.")
+edad = int(input("¿Cuántos años tienes? "))
+
+if edad >= 18:
+    print("Hola", nombre, "eres mayor de edad.")
+else:
+    print("Hola", nombre, "eres menor de edad.")

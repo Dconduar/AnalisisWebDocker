0a15185d929a4e18219aa210f7da93f093a9ed60
Diego Alexander Contreras Duarte <dcontrerasd>
2025-08-01 14:27:42 -0600

    Version 5: agrega funcion y comentarios

--- git-ejemplo/programa.py
+++ git-ejemplo/programa.py
@@ -1,8 +1,13 @@
-print("Bienvenido al programa de Diego")
-nombre = input("¿Cómo te llamas? ")
-edad = int(input("¿Cuántos años tienes? "))
+# Programa interactivo básico hecho por Diego
 
-if edad >= 18:
-    print("Hola", nombre, "eres mayor de edad.")
-else:
-    print("Hola", nombre, "eres menor de edad.")
+def saludar():
+    nombre = input("¿Cómo te llamas? ")
+    edad = int(input("¿Cuántos años tienes? "))
+    
+    if edad >= 18:
+        print("Hola", nombre, "eres mayor de edad.")
+    else:
+        print("Hola", nombre, "eres menor de edad.")
+
+print("Bienvenido al programa de Diego")
+saludar()
